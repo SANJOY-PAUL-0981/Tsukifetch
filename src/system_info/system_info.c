225a8a1230c6cf7bf6eee8f2fa72b01d91f5a31d
Sanjoy Paul <paulsanjoy2005@gmail.com>
2024-07-29 19:55:48 +0530

    directory created

---
+++ src/system_info/system_info.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "system_info.h"
+
+// Read distro name
+char *readDistroName()
+{
+    char *distro;
+    FILE *infoFile;
+    char *line = NULL;
+    size_t n = 0;
+
+    infoFile = fopen("/etc/os-release", "r");
+
+    if (infoFile == NULL)
+    {
+        perror("Error in opening /etc/os-release");
+        return NULL;
+    }
+
+    while (getline(&line, &n, infoFile) != -1)
+    {
+        if (strncmp(line, "ID=", 3) == 0)
+        {
+            distro = line + 3;
+            break;
+        }
+    }
+
+    fclose(infoFile);
+    free(line);
+    return distro;
+}
+
+// Read CPU info
+char *CPU_info(void)
+{
+    char *cp;
+    FILE *cpuInfo;
+    char *line = NULL;
+    size_t n = 0;
+    cpuInfo = fopen("/proc/cpuinfo", "r");
+    if (cpuInfo == NULL)
+    {
+        perror("Error opening /proc/cpuinfo");
+        return NULL;
+    }
+    while (getline(&line, &n, cpuInfo) != -1)
+    {
+        if (strncmp(line, "model name :", 10) == 0)
+        {
+            cp = line + 13;
+            break;
+        }
+    }
+
+    fclose(cpuInfo);
+    free(line);
+    return cp;
+}
---
+++ src/system_info/system_info.h
@@ -0,0 +1,9 @@
+#ifndef SYSTEM_INFO_H
+#define SYSTEM_INFO_H
+
+#include <sys/utsname.h>
+
+char* readDistroName(void);
+char* CPU_info(void);
+
+#endif // SYSTEM_INFO_H
