27f115f8b9ccb1445ffc6c8bc12706783ec2e888
HARAJIT <110291590+HARAJIT05@users.noreply.github.com>
2024-07-31 11:43:20 +0000

    Fixed

--- src/tsuki.c
+++ src/tsuki.c
@@ -1,54 +1,90 @@
-// C Header Files
-#include <sys/utsname.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "ASCII_art/ascii_art.h"
-#include "sys_info/system_info.h"
+#include <unistd.h>
+#include <sys/utsname.h>
+#include <sys/sysinfo.h>
 
-// ANSI colour codes
-#define RED "\e[0;31m"
-#define LIGHT_RED "\033[1;31m"
-#define CYAN "\033[1;36m"
-#define BLUE "\033[1;34m"
-#define YELLOW "\033[1;33m"
-#define GREEN "\033[0;32m"
-#define WHITE "\033[1;37m"
-#define LIGHT_GRAY "\033[0;37m"
-#define RESET "\033[0m"
-#define PURPLE "\e[0;35m"
-#define BOLD_RED "\e[1;31m"
-#define BOLD_PURPLE "\e[1;35m"
-#define ORANGE "\033[38;5;202m"
-#define PINK "\033[38;5;206m"
+void print_ascii_art(const char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        perror("Error opening file");
+        return;
+    }
 
+    char ch;
+    while ((ch = fgetc(file)) != EOF) {
+        putchar(ch);
+    }
+    fclose(file);
+}
 
-int main(int argc, char const *argv[])
-{
-    // uname struct declaration
+void print_system_info() {
     struct utsname uts;
-    uname(&uts);
-    char *userName = getenv("USER");
-    char *shellName = getenv("SHELL");
-    char *distroName = readDistroName();
-    char *cpuStats = CPU_info();
+    if (uname(&uts) < 0) {
+        perror("uname");
+        return;
+    }
+    printf("OS: %s %s\n", uts.sysname, uts.release);
+}
 
-    // ASCII art print
-    printASCII_Logo(distroName);
+void print_memory_info() {
+    struct sysinfo info;
+    if (sysinfo(&info) < 0) {
+        perror("sysinfo");
+        return;
+    }
+    printf("Memory: %ld MB / %ld MB\n", (info.totalram - info.freeram) / 1024 / 1024, info.totalram / 1024 / 1024);
+}
 
-    // info printing
-    printf(BOLD_PURPLE "\n\n 👤 USER: " WHITE "%s\n" RESET, userName);
-    printf(BOLD_PURPLE " 📦 DISTRO: " WHITE "%s" RESET, distroName);
-    printf(BOLD_PURPLE " 🌐 HOST: " WHITE "%s\n" RESET, uts.nodename);
-    printf(BOLD_PURPLE " 📟 SHELL: " WHITE "%s\n" RESET, shellName);
-    printf(BOLD_PURPLE " 💾 KERNEL: " WHITE "%s\n" RESET, uts.release);
+void show_os_art() {
+    struct utsname uts;
+    if (uname(&uts) < 0) {
+        perror("uname");
+        return;
+    }
 
-    // Desktop Env getenv() lib func
-    char *desktop_Enviorment = getenv("XDG_CURRENT_DESKTOP");
-    printf(BOLD_PURPLE " 🏙️  DE/WM: " WHITE "%s\n" RESET, desktop_Enviorment);
-    printf(BOLD_PURPLE " 🗄️  CPU: " WHITE "%s" RESET, cpuStats);
+    printf("Displaying ASCII Art:\n");
+    printf("---------------------------------\n");
 
-    printf(CYAN "\n🭸🭸🭸🭬🭸🭸🭸🭬🭸🭸🭸🭬🭸🭸🭸🭬🭸🭸🭸🭬🭸🭸🭸🭬🭸🭸🭸🭬\n" RESET);
-    printf(YELLOW "> tsukishima - built by tsukishima\n" RESET);
+    if (strcmp(uts.sysname, "Linux") == 0) {
+        if (strstr(uts.release, "arch") != NULL) {
+            print_ascii_art("ASCII_art/logo/arch.txt");
+        } else if (strstr(uts.release, "debian") != NULL) {
+            print_ascii_art("ASCII_art/logo/debian.txt");
+        } else if (strstr(uts.release, "fedora") != NULL) {
+            print_ascii_art("ASCII_art/logo/fedora.txt");
+        } else if (strstr(uts.release, "kubuntu") != NULL) {
+            print_ascii_art("ASCII_art/logo/kubuntu.txt");
+        } else if (strstr(uts.release, "linuxmint") != NULL) {
+            print_ascii_art("ASCII_art/logo/linux_mint.txt");
+        } else if (strstr(uts.release, "ubuntu") != NULL) {
+            print_ascii_art("ASCII_art/logo/ubuntu.txt");
+        } else {
+            print_ascii_art("ASCII_art/logo/nothing.txt");
+        }
+    } else if (strcmp(uts.sysname, "Darwin") == 0) {
+        print_ascii_art("ASCII_art/logo/mac.txt");
+    } else if (strcmp(uts.sysname, "Windows") == 0) {
+        print_ascii_art("ASCII_art/logo/windows.txt"); // Example, ensure you have this file
+    } else {
+        print_ascii_art("ASCII_art/logo/nothing.txt");
+    }
+
+    printf("\n---------------------------------\n");
+}
+
+int main() {
+    printf("Welcome to Tsukifetch!\n");
+
+    // Display the ASCII art based on the OS
+    show_os_art();
+
+    // Print system information
+    print_system_info();
+    printf("---------------------------------\n");
+    print_memory_info();
+    printf("---------------------------------\n");
+
     return 0;
 }
