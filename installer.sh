827bedf5b031ade1c5d1871c2b01223d6f2a8f24
Sanjoy Paul <paulsanjoy2005@gmail.com>
2024-08-01 10:56:39 +0530

    installer updated and almost done

--- installer.sh
+++ installer.sh
@@ -2,7 +2,7 @@
 
 gcc -Isrc src/tsuki.c src/ASCII_art/ascii_art.c src/sys_info/system_info.c -o tsukifetch
 sudo mkdir -p /usr/local/share/tsukifetch/logos/
-sudo cp Tsukifetch/src/logo/* /usr/local/share/tsukifetch/logos/
+sudo cp src/logo/* /usr/local/share/tsukifetch/logos/
 
 sudo cp tsukifetch /usr/local/bin/
 sudo chmod +x /usr/local/bin/tsukifetch
